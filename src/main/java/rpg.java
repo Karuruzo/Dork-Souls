1311d3262ac74c40fc75e3950a462ae6e7ccb5f3
Carlos <chenriquep6@gmail.com>
2022-04-26 12:29:31 -0300

    RPG

---
+++ src/main/java/rpg.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class rpg {
+    public static void main(String[] args) {
+        Scanner insert = new Scanner(System.in);
+
+        System.out.print("##--      DORK SOULS     --##\n\n");
+        System.out.print("|-----------------------------|\n");
+        System.out.print("| Opção 1 - Jogar             |\n");
+        System.out.print("| Opção 2 -           |\n");
+        System.out.print("| Opção 3 - Produtos          |\n");
+        System.out.print("| Opção 4 - Sair              |\n");
+        System.out.print("|-----------------------------|\n");
+        System.out.print("Digite uma opção: ");
+
+        int opcao = insert.nextInt();
+
+
+    }
+}
